69fc570cdad0a2ecdd24190cd4bbcaa6c5c74322
Boris Plotnikov <boriswo@gmail.com>
2026-05-24 20:49:17 -0700

    Add Health Router

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import healthRouter from "./routes/health.routes.js";
 
 const app = express();
 
@@ -7,9 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("./api", healthRouter); // Mounts the route at /api/health
+
 // Basic health check route
-app.get("/api/health", (req, res) => {
-  res.json({ status: "OK", message: "Backend is running smoothly" });
-});
+// app.get("/api/health", (req, res) => {
+//   res.json({ status: "OK", message: "Backend is running smoothly" });
+// });
 
 export default app;
