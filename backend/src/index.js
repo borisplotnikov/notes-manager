2f13aa52ac5e42639b1cc98df68421809563c3cc
Boris Plotnikov <boriswo@gmail.com>
2026-05-24 20:26:41 -0700

    Add app.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,13 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
+import app from "./app.js";
 import connectDB from "./config/db.js";
 
 // Load environment variables
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 connectDB();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Basic health check route
-app.get("/api/health", (req, res) => {
-  res.json({ status: "OK", message: "Backend is running smoothly" });
-});
 
 // Start server
 app.listen(PORT, () => {
