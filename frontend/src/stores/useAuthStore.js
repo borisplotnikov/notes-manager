0c38c5429ba646225e533a727b949a74dfea70ff
Boris Plotnikov <boriswo@gmail.com>
2026-06-23 12:50:47 -0700

    Configure Zustand Persist

--- frontend/src/stores/useAuthStore.js
+++ frontend/src/stores/useAuthStore.js
@@ -1,25 +1,33 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useAuthStore = create((set) => ({
-  // Initial State
-  user: null,
-  token: null,
-  isAuthenticated: false,
-
-  // Actions
-  login: (user, token) =>
-    set({
-      user,
-      token,
-      isAuthenticated: true,
-    }),
-
-  logout: () =>
-    set({
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      // Initial State
       user: null,
       token: null,
       isAuthenticated: false,
+
+      // Actions
+      login: (user, token) =>
+        set({
+          user,
+          token,
+          isAuthenticated: true,
+        }),
+
+      logout: () =>
+        set({
+          user: null,
+          token: null,
+          isAuthenticated: false,
+        }),
     }),
-}));
+    {
+      name: "notes-auth-storage", // Unique key for localStorage
+    },
+  ),
+);
 
 export default useAuthStore;
